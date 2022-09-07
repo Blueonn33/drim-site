cf2aa5d50a45b9d308ab9fd75b82805752e0ddd5
Мартин Маринов <mm92469142@edu.mon.bg>
2022-09-07 16:08:40 +0300

    styles_js

---
+++ javas.js
@@ -0,0 +1,228 @@
+$(document).ready(function(){
+    $("#sbutton1").click(function(){
+        $("#pro1").css({
+        height: '300px',
+        width: '300px',
+        right:'20px'
+        },"slow"),
+        $("#hbutton1").css({
+            visibility: 'visible'
+        }),$("#sbutton1").css({
+            visibility: 'hidden'
+        });
+    });
+    });
+$(document).ready(function(){
+    $("#hbutton1").click(function(){
+        $("#pro1").css({
+        height: '200px',
+        width: '200px',
+        right:'0px'
+        },"slow"),
+        $("#hbutton1").css({
+            visibility: 'hidden',
+            right: '20px'
+        }),
+        $("#sbutton1").css({
+            visibility: 'visible',
+        });
+    });
+    });
+
+$(document).ready(function(){
+    $("#sbutton2").click(function(){
+        $("#pro2").css({
+        height: '300px',
+        width: '300px',
+        right:'20px'
+        },"slow"),
+        $("#hbutton2").css({
+            visibility: 'visible'
+        }),$("#sbutton2").css({
+            visibility: 'hidden'
+        });
+    });
+    });
+    $(document).ready(function(){
+        $("#hbutton2").click(function(){
+            $("#pro2").css({
+            height: '200px',
+            width: '200px',
+            right:'0px'
+            },"slow"),
+            $("#hbutton2").css({
+                visibility: 'hidden',
+                right: '20px'
+            }),
+            $("#sbutton2").css({
+                visibility: 'visible',
+            });
+        });
+        });
+
+    $(document).ready(function(){
+        $("#sbutton3").click(function(){
+            $("#pro3").css({
+            height: '300px',
+            width: '300px',
+            right:'20px'
+            },"slow"),
+            $("#hbutton3").css({
+                visibility: 'visible'
+            }),$("#sbutton3").css({
+                visibility: 'hidden'
+            });
+        });
+        });
+    $(document).ready(function(){
+        $("#hbutton3").click(function(){
+            $("#pro3").css({
+            height: '200px',
+            width: '200px',
+            right:'0px'
+            },"slow"),
+            $("#hbutto3").css({
+                visibility: 'hidden',
+                right: '20px'
+            }),
+            $("#sbutton3").css({
+                visibility: 'visible',
+            });
+        });
+        });
+
+    $(document).ready(function(){
+        $("#sbutton4").click(function(){
+            $("#pro4").css({
+            height: '300px',
+            width: '300px',
+            right:'20px'
+            },"slow"),
+            $("#hbutton4").css({
+                visibility: 'visible'
+            }),$("#sbutton4").css({
+                visibility: 'hidden'
+            });
+        });
+        });
+    $(document).ready(function(){
+        $("#hbutton4").click(function(){
+            $("#pro4").css({
+            height: '200px',
+            width: '200px',
+            right:'0px'
+            },"slow"),
+            $("#hbutto4").css({
+                visibility: 'hidden',
+                right: '20px'
+            }),
+            $("#sbutton4").css({
+                visibility: 'visible',
+            });
+        });
+        });
+
+
+    $(document).ready(function(){
+        $("#sbutton5").click(function(){
+            $("#pro5").css({
+            height: '300px',
+            width: '300px',
+            right:'20px'
+            },"slow"),
+            $("#hbutton5").css({
+                visibility: 'visible'
+            }),$("#sbutton5").css({
+                visibility: 'hidden'
+            });
+        });
+        });
+    $(document).ready(function(){
+        $("#hbutton5").click(function(){
+            $("#pro5").css({
+            height: '200px',
+            width: '200px',
+            right:'0px'
+            },"slow"),
+            $("#hbutton5").css({
+                visibility: 'hidden',
+                right: '20px'
+            }),
+            $("#sbutton5").css({
+                visibility: 'visible',
+            });
+        });
+        });
+
+
+    $(document).ready(function(){
+        $("#sbutton6").click(function(){
+            $("#pro6").css({
+            height: '300px',
+            width: '300px',
+            right:'20px'
+            },"slow"),
+            $("#hbutton6").css({
+                visibility: 'visible'
+            }),$("#sbutton6").css({
+                visibility: 'hidden'
+            });
+        });
+        });
+    $(document).ready(function(){
+        $("#hbutton6").click(function(){
+            $("#pro6").css({
+            height: '200px',
+            width: '200px',
+            right:'0px'
+            },"slow"),
+            $("#hbutton6").css({
+                visibility: 'hidden',
+                right: '20px'
+            }),
+            $("#sbutton6").css({
+                visibility: 'visible',
+            });
+        });
+        });
+
+    function secondPage() {
+        location.assign("siteHTML2.html");
+        }
+
+    function firstPage() {
+        location.assign("siteHTML.html");
+        }
+    
+
+    // инфо бутони
+
+    $(document).ready(function(){
+        $("#ibutton1").click(function(){
+            $("#info1").css({
+                // left:'-400px'
+            },"slow"),
+            $("#info1").css({
+                display: 'unset'
+            }),
+            $('.prod1').css({
+                display: 'none'
+            });
+        });
+        });
+    // $(document).ready(function(){
+    //     $("#ibutton1").click(function(){
+    //         $("#pro1").css({
+    //         height: '200px',
+    //         width: '200px',
+    //         right:'0px'
+    //         },"slow"),
+    //         $("#hbutton1").css({
+    //             visibility: 'hidden',
+    //             right: '20px'
+    //         }),
+    //         $("#sbutton1").css({
+    //             visibility: 'visible',
+    //         });
+    //     });
+    //     });
